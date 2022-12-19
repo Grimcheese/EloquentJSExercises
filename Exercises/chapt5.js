a05e203b8c063c5e92a58f4c89fc0719b801a9b0
Alby Chawula <albychawula@gmail.com>
2022-12-19 18:18:26 +0800

    Extra comments to describe what exercises are being completed.

--- Exercises/chapt5.js
+++ Exercises/chapt5.js
@@ -1,6 +1,19 @@
+// Question 1: Flattening ------------------
+
+/** Use reduce method to flatten an array of arrays into a single array.
+ *  Combine reduce with concat
+ */
+
 testArr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 testArr.reduce((a, b) => (a.concat(b))); // Flatten testArr
 
+
+// Question 2: Your Own Loop -----------------
+
+/** Write a higher order function loop that provides similar functionality to a
+ *  for loop.
+ *  It takes a value, test function, update function and a body.
+ */
 function loop(value, test, update, body) {
     while (test(value)) {
         body(value);
@@ -8,8 +21,19 @@ function loop(value, test, update, body) {
     }
 }
 
+// Execute loop()
 loop(1, a => a < 10, a => a + 1, console.log);
 
+// Question 3: Everything -------------------
+
+/** Implement an every function. One that uses a loop and one that uses some().
+ * 
+ *  everySome() utilises De Morgan's Laws to convert && operations (every) to
+ *  || (some) and vice versa. The formula is a && b == !(!a || !b)
+ * 
+ *  everySome works by taking the negation of the test on each element then 
+ *  negating the entire || operation. !(some(!a || !b || !c, etc...))
+ */
 function everyLoop(inArr, test) {
     for (element of inArr) {
         if (!test(element)) {
