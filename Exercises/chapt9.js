e9141e9def0c27960cabcdef8dec646b467f8828
Alby Chawula <albychawula@gmail.com>
2023-01-13 20:10:58 +0800

    Started chapter 9 exercises on regular expressions.

---
+++ Exercises/chapt9.js
@@ -0,0 +1,12 @@
+/** Chapter 9: Regular Expressions */
+
+/** Question 1: Regexp Golf */
+
+/** Match car and cat */
+
+regext = /ca[tr]/;
+regext.match("car");
+
+/** Match pop and prop */
+
+regext = /p(r*)op/
